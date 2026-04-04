0ebcbf1c688f2fb59f66408ade5e543fae50bae0
sonpham <sonviruss2001@gmail.com>
2026-04-04 22:47:18 +0700

    update footer user

--- fe-react/src/layouts/user/index.jsx
+++ fe-react/src/layouts/user/index.jsx
@@ -1,5 +1,14 @@
+import Footer from "./footer";
+import Header from "./header";
+
 const UserLayout = ({ children }) => {
-  return <div className="min-h-screen bg-white">{children}</div>;
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default UserLayout;
