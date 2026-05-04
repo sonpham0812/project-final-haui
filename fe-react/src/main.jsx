8057195d0644c74da050cbf1ee7a1466371b11f8
sonpham0812 <sonviruss2001@gmail.com>
2026-05-04 23:21:32 +0700

    fix main.jsx

--- fe-react/src/main.jsx
+++ fe-react/src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 import App from "./App.jsx";
 
