a5e78f5db2cd8344b5b6478247916f2c849ffa34
sonpham <sonviruss2001@gmail.com>
2026-05-28 13:31:33 +0700

    update schema, fix search

--- be-express/src/config/db.js
+++ be-express/src/config/db.js
@@ -11,6 +11,14 @@ const pool = mysql2.createPool({
   connectionLimit:   10,
   queueLimit:        0,
   charset:           'utf8mb4',
+  // Đảm bảo mỗi connection đều dùng utf8mb4 - quan trọng cho tiếng Việt
+  // Đặc biệt cần thiết khi deploy trên Railway
+});
+
+// Chạy SET NAMES ngay khi mỗi connection mới được tạo
+// Đây là cách chắc chắn nhất để đảm bảo charset đúng với mysql2
+pool.pool.on('connection', (connection) => {
+  connection.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
 });
 
 module.exports = pool;
